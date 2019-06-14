3632cdfe78afac8b3c102c5a8adce9a2e130e8f6
vmarconcini7 <vinicius.marconcini@gmail.com>
2019-06-14 19:54:03 -0300

    continuation

---
+++ app/src/main/java/com/example/boaviagem/Configuracoes.kt
@@ -0,0 +1,23 @@
+package com.example.boaviagem
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Configuracoes : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.config, container, false)
+    }
+
+
+}
--- app/src/main/java/com/example/boaviagem/MainActivity.kt
+++ app/src/main/java/com/example/boaviagem/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.boaviagem
 
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationMenu
+import android.support.design.widget.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.framePrincipal, NovaViagem())
+            .commit()
+
+        findViewById<BottomNavigationView>(R.id.navegacao)
+            .setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.itemA -> createFragment(NovaViagem())
+                    R.id.itemB -> createFragment(NovoGasto())
+                    R.id.itemC -> createFragment(MinhasViagens())
+                    R.id.itemD -> createFragment(Configuracoes())
+                    else -> false
+                }
+            }
+
+    }
+
+    private fun createFragment(f: android.support.v4.app.Fragment): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.framePrincipal, f)
+            .addToBackStack(null)
+            .commit()
+        return true
     }
 }
