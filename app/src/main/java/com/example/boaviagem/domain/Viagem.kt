622a988fcdfd194be3bca24c80c6ddab16c514c1
vmarconcini7 <48968256+vmarconcini7@users.noreply.github.com>
2019-06-28 00:14:01 -0300

    continuation

--- app/src/main/java/com/example/boaviagem/domain/Viagem.kt
+++ app/src/main/java/com/example/boaviagem/domain/Viagem.kt
@@ -9,8 +9,8 @@ data class Viagem(val destino: String,
                   val dataIda: String,
                   val dataVolta: String,
                   val tipoViagem: String,
-                  val orcamento: Float,
-                  val qtPessoas: Int) {
+                  val orcamento: String,
+                  val qtPessoas: String) {
 
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
