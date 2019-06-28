038ed465955c58322e31d4bcb305267a33491a7d
vmarconcini7 <48968256+vmarconcini7@users.noreply.github.com>
2019-06-27 23:52:00 -0300

    continuation

--- app/src/main/java/com/example/boaviagem/NovaViagem.kt
+++ app/src/main/java/com/example/boaviagem/NovaViagem.kt
@@ -1,5 +1,6 @@
 package com.example.boaviagem
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +13,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import android.text.Editable
+import android.util.Log
 
 /**
  * A simple [Fragment] subclass.
@@ -23,39 +27,75 @@ class NovaViagem : androidx.fragment.app.Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.nova_viagem, container, false)
+        
+        val layout = inflater.inflate(R.layout.nova_viagem, container, false)
 
-        val dtPartida: Button  = findViewById(R.id.dtPartida);
-        val dtVolta: Button  = findViewById(R.id.dtVolta);
-        val btnSalvar: Button = findViewById(R.id.btnSalvar)
+        val dtPartida = layout.findViewById<Button>(R.id.dtPartida)
+        val dtVolta = layout.findViewById<Button>(R.id.dtVolta)
+        val btnSalvar = layout.findViewById<Button>(R.id.btnSalvar)
+        val dsDestino = layout.findViewById<Spinner>(R.id.dsDestino)
+        var controlFieldDate = "1";
+        
+        fun updateDateInView() {
+
+            val myFormat = "dd/MM/yyyy"
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            if(controlFieldDate.equals("1")) {
+                dtPartida.text = Editable.Factory.getInstance().newEditable(sdf.format(cal.time))
+            }else{
+                dtVolta.text = Editable.Factory.getInstance().newEditable(sdf.format(cal.time))
+            }
+        }
+        
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+        
+        dtPartida.setOnClickListener {
+            controlFieldDate = "1";
+            DatePickerDialog(this@NovaViagem.context,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
+        
+        dtVolta.setOnClickListener {
+            controlFieldDate = "2";
+            DatePickerDialog(this@NovaViagem.context,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
 
         btnSalvar.setOnClickListener {
             salvar()
         }
-
-        dtPartida.setOnClickListener {
-            showDatePickerDialog()
-        }
-
-        dtVolta.setOnClickListener {
-            showDatePickerDialog()
-        }
     }
 
-    fun salvar() {
+    fun salvar(destino: String,
+               dataIda: String,
+               dataVolta: String,
+               tipoViagem: String,
+               orcamento: Int,
+               qtPessoas: Int) {
         GlobalScope.launch(Dispatchers.Main) {
             val contatos = withContext(Dispatchers.IO) {
                 this@NovaViagem.context?.also {
                     val viagemDao = dbViagem.getInstance(it).viagemDao()
-                    val viagem = Viagem("xxxx", "xxxx", "xxxxx", "xxx", 10F, 1)
+                    val viagem = Viagem(destino, dataIda, dataVolta, tipoViagem, orcamento, qtPessoas)
                     viagemDao.inserir(viagem)
                 }
             }
         }
-    }
-
-    fun showDatePickerDialog() {
-        val newFragment = DatePickerFragment()
-        newFragment.show(fragmentManager, "datePicker")
     }
 }
